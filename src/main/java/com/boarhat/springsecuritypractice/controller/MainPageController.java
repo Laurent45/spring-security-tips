f73208662d0cca6260819b97b9320cbc739a569b
Laurent FREDERICK <laurent.frederick@icloud.com>
2026-01-18 15:57:24 +0100

    Set up main page

---
+++ src/main/java/com/boarhat/springsecuritypractice/controller/MainPageController.java
@@ -0,0 +1,24 @@
+package com.boarhat.springsecuritypractice.controller;
+
+import com.boarhat.springsecuritypractice.service.ProductService;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPageController {
+
+    private final ProductService productService;
+
+    public MainPageController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/main")
+    public String mainPage(Authentication authentication, Model model) {
+        model.addAttribute("username", authentication.getName());
+        model.addAttribute("products", productService.findAll());
+        return "main";
+    }
+}
