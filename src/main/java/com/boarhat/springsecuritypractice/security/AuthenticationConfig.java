0b7a1062f55c9e2f076e6daccdea16b00dc88520
Laurent FREDERICK <laurent.frederick@icloud.com>
2026-01-18 15:11:36 +0100

    Set up authentication configuration

---
+++ src/main/java/com/boarhat/springsecuritypractice/security/AuthenticationConfig.java
@@ -0,0 +1,19 @@
+package com.boarhat.springsecuritypractice.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+@Configuration
+public class AuthenticationConfig {
+    private final AuthenticationProviderService authenticationProvider;
+
+    public AuthenticationConfig(AuthenticationProviderService authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Autowired
+    void  configureAuthenticationManager(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider);
+    }
+}
