38b85957d6511eaee98a4de99b40bee73835cf84
Laurent FREDERICK <laurent.frederick@icloud.com>
2026-01-18 14:47:10 +0100

    Set up a custom user details service

---
+++ src/main/java/com/boarhat/springsecuritypractice/security/JpaUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.boarhat.springsecuritypractice.security;
+
+import com.boarhat.springsecuritypractice.repository.UserRepository;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public JpaUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public @NonNull CustomUserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+}
