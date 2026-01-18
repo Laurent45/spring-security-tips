fa9b5672ea2fce19d423c1bed107d808dc2d96a1
Laurent FREDERICK <laurent.frederick@icloud.com>
2026-01-18 14:46:54 +0100

    Setup two password encoders

---
+++ src/main/java/com/boarhat/springsecuritypractice/security/PasswordEncoderConfig.java
@@ -0,0 +1,20 @@
+package com.boarhat.springsecuritypractice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoderConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SCryptPasswordEncoder scryptPasswordEncoder() {
+        return SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+}
