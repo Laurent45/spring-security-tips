a09914a5d582481bc60afbe73d1331444d14763f
Laurent FREDERICK <laurent.frederick@icloud.com>
2026-02-02 12:11:06 +0100

    Example of a simple custom filter before a specific filter

--- src/main/java/com/boarhat/springsecuritypractice/security/configuration/SecurityFilterConfig.java
+++ src/main/java/com/boarhat/springsecuritypractice/security/configuration/SecurityFilterConfig.java
@@ -1,10 +1,12 @@
 package com.boarhat.springsecuritypractice.security.configuration;
 
+import com.boarhat.springsecuritypractice.security.filter.RequestValidationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -13,12 +15,14 @@ public class SecurityFilterConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http
-                .formLogin(formLogin -> {
+                .addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class)
+/*                .formLogin(formLogin -> {
                     formLogin.defaultSuccessUrl("/main", true);
                 })
                 .authorizeHttpRequests(authorizeRequests -> {
                     authorizeRequests.anyRequest().authenticated();
-                })
+                })*/
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .build();
     }
 }
